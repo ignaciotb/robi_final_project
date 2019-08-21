3d104fc943240826ed28514b72fdb16f4022eb87
Torroba <torroba@kth.se>
2019-08-21 14:43:23 +0200

    Installing python scripts from pkg in pythonpath

--- robotics_project/setup.py
+++ robotics_project/setup.py
@@ -1,11 +1,11 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-  scripts=[	'scripts/state_machines/sm_students.py', 
-  		   	'scripts/utils/poses_pub.py', 
-  		   	'scripts/utils/gazebo_models_hdl.py', 
-  		   	'scripts/demo/manipulation_server.py',
-        	'scripts/demo/manipulation_client.py',
-        	'scripts/demo/grasps_server.py'])
+    packages=['robotics_project'],
+    package_dir={'': 'src'}
+)
+
 setup(**d)
